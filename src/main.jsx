ca551cfb31b4916210c332ec115ae75d29b6c733
jatin <jatins2732@gmail.com>
2024-12-23 18:36:40 +0530

    second

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // import './App.css'
-import App from "./App"
+import App from "../App";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <App/>
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
